611e0a5a3b636a23b7bab9807a21dcd467c5a6ab
bintangsuryanata <bintangsurya.bs18@gmail.com>
2023-12-09 19:29:31 +0800

    first commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -58,7 +58,7 @@ const Portfolio = () => {
           <p className="py-6">Check out some of my work right here</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-10 mb-10 px-12 sm:px-0">
           {portfolios.map(({ id, src }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-// index.js
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/gustibintangs">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );

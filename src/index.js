88240c731c7787152c21330100ef2e76b88128b0
bintangsuryanata <bintangsurya.bs18@gmail.com>
2023-12-09 19:07:27 +0800

    Update Index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+// index.js
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/gustibintangs">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
